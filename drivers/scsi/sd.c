6671fa9dc0b403fed05c5f33d5be0194ec2df0b6
Hannes Reinecke <hare@suse.de>
2021-02-23 17:28:55 +0800

    block: genhd: add 'groups' argument to device_add_disk
    commit fef912bf860e8e7e48a2bfb978a356bba743a8b7 upstream.
    
    Update device_add_disk() to take an 'groups' argument so that
    individual drivers can register a device with additional sysfs
    attributes.
    This avoids race condition the driver would otherwise have if these
    groups were to be created with sysfs_add_groups().
    
    Signed-off-by: Martin Wilck <martin.wilck@suse.com>
    Signed-off-by: Hannes Reinecke <hare@suse.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3466,7 +3466,7 @@ static void sd_probe_async(void *data, async_cookie_t cookie)
 		msleep(500);
 	}
 #endif
-	device_add_disk(dev, gd);
+	device_add_disk(dev, gd, NULL);
 #ifdef CONFIG_USB_STORAGE_DETECT
 	sdkp->prv_media_present = sdkp->media_present;
 #endif
