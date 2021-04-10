2e28b04fd13bf630a86aa0e363a5a9bf5c38e48a
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2021-04-10 13:39:13 +0900

    cnss2: Fixed the difference with SM-G986D_JPN_QQ_Opensource

--- include/net/cnss2.h
+++ include/net/cnss2.h
@@ -243,7 +243,4 @@ extern int cnss_set_fw_log_mode(struct device *dev, uint8_t fw_log_mode);
 extern int cnss_set_pcie_gen_speed(struct device *dev, u8 pcie_gen_speed);
 extern int cnss_pci_get_reg_dump(struct device *dev, uint8_t *buffer,
 				 uint32_t len);
-
-extern int cnss_sysfs_get_pm_info(void);
-extern void cnss_sysfs_update_driver_status(int32_t new_status, void *version, void *softap);
 #endif /* _NET_CNSS2_H */
