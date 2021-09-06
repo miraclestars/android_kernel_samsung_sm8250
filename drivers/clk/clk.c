0e53292fdda34a37a914ddb3f8af71c9379b8260
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-09-06 10:31:20 +0200

    clk: fix build warning for orphan_list
    In the backport of commit bdcf1dc25324 ("clk: Evict unregistered clks
    from parent caches") to the 4.19.y and 4.14.y stable trees, the
    orphan_list structure was placed in the wrong location, causing loads of
    build warnings on systems that do not define CONFIG_DEBUG_FS.
    
    Fix this up by moving the structure to the correct place in the file.
    
    Reported-by: "kernelci.org bot" <bot@kernelci.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -71,11 +71,6 @@ static struct hlist_head *all_lists[] = {
 	NULL,
 };
 
-static struct hlist_head *orphan_list[] = {
-	&clk_orphan_list,
-	NULL,
-};
-
 /***    private data structures    ***/
 
 struct clk_core {
@@ -3243,6 +3238,11 @@ static int inited = 0;
 static u32 debug_suspend = 1;
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
+
+static struct hlist_head *orphan_list[] = {
+	&clk_orphan_list,
+	NULL,
+};
 
 static void clk_state_subtree(struct clk_core *c)
 {
